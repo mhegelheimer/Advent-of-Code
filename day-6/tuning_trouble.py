6106bf7dc2e9236e82ed813a4834aa50c6ebd2b8
Michael Hegelheimer <michael.hegelheimer@gmail.com>
2022-12-23 00:20:14 -0600

    day 6, pt 2. easiest one so far

--- day-6/tuning_trouble.py
+++ day-6/tuning_trouble.py
@@ -1,8 +1,11 @@
 def main():
-    # data = "bvwbjplbgvbhsrlpgdmjqwftvncz"
-    # data = "nppdvjthqldpwncqszvftbrmjlhg"
-    # data = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
-    # data = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    # part 1 examples
+    # data = "bvwbjplbgvbhsrlpgdmjqwftvncz"         # p1: 5, p2: 23
+    # data = "nppdvjthqldpwncqszvftbrmjlhg"         # p1: 6, p2: 23
+    # data = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"    # p1: 10, p2: 29
+    # data = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"     # p1: 11, p2: 26
+    # data = "mjqjpqmgbljsphdztnvjfqwrcgsmlb"       # p2: 19
+
     with open("input.txt") as fh:
         data = fh.readlines()[0].strip()
 
@@ -10,7 +13,8 @@ def main():
     for idx, c in enumerate(data):
 
         # escape case (also conveniently using next idx to cheat 1-indexed problem statement)
-        if len(marker) == 4:
+        # if len(marker) == 4:
+        if len(marker) == 14:
             return marker, idx
 
         if c in marker:
