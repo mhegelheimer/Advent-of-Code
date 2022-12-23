1c5f0f50243c7f43801e85ab47529eb5667f7eeb
Michael Hegelheimer <michael.hegelheimer@gmail.com>
2022-12-23 00:12:57 -0600

    day 6, pt1. quick one so far

---
+++ day-6/tuning_trouble.py
@@ -0,0 +1,32 @@
+def main():
+    # data = "bvwbjplbgvbhsrlpgdmjqwftvncz"
+    # data = "nppdvjthqldpwncqszvftbrmjlhg"
+    # data = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
+    # data = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
+    with open("input.txt") as fh:
+        data = fh.readlines()[0].strip()
+
+    import pdb
+
+    pdb.set_trace()
+
+    marker = []
+    for idx, c in enumerate(data):
+
+        # escape case (also conveniently using next idx to cheat 1-indexed problem statement)
+        if len(marker) == 4:
+            return marker, idx
+
+        if c in marker:
+            while c in marker:
+                marker = marker[1:]
+            marker.append(c)
+            continue
+
+        else:
+            marker.append(c)
+
+
+if __name__ == "__main__":
+    marker, idx = main()
+    print(f"Index of end of 4 unique chars ({marker}) is {idx}")
